78cf1abe40c93a02ae2fff823174ba3887d456b7
ryudai <bigdragon.06102001@gmail.com>
2023-01-21 01:53:40 +0900

    legal_policy カスタムタイプを作成

--- .slicemachine/assets/slices/Paragraph/index.stories.js
+++ .slicemachine/assets/slices/Paragraph/index.stories.js
@@ -5,5 +5,5 @@ export default {
 }
 
 
-export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{"content":[{"type":"paragraph","text":"Culpa in Lorem non do in sit mollit sunt sit laborum. Veniam reprehenderit nulla in. Anim ullamco ullamco reprehenderit.","spans":[]}]}],"primary":{},"id":"_Default","slice_type":"paragraph"}} />
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{"content":[{"type":"paragraph","text":"Eu ad aute commodo pariatur quis. Commodo deserunt nulla ex sit elit eu anim cupidatat commodo ullamco enim esse proident cillum enim.","spans":[]}]}],"primary":{},"id":"_Default","slice_type":"paragraph"}} />
 _Default.storyName = ''
