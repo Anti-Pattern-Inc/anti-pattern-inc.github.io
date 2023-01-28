d8144b16c56451f1952722b2a2cc78538c328ffb
ryudai <bigdragon.06102001@gmail.com>
2023-01-29 08:34:58 +0900

    Heroの文言を全てh1に含めた

--- src/components/top/Hero.tsx
+++ src/components/top/Hero.tsx
@@ -8,11 +8,13 @@ const Hero = () => {
         <Image src={HeroImg} alt="hero_img" className="h-full object-cover" />
       </div>
       <div className="relative mx-auto max-w-7xl px-6 lg:px-8 pt-52">
-        <p className="mb-4 text-3xl font-bold text-center">
-          日本のソフトウェアエンジニアを
-        </p>
-        <h1 className="text-4xl font-bold sm:text-5xl lg:text-6xl text-center">
-          憧れの職業へ
+        <h1>
+          <span className="block mb-4 text-3xl font-bold text-center">
+            日本のソフトウェアエンジニアを
+          </span>
+          <span className="block text-4xl font-bold sm:text-5xl lg:text-6xl text-center">
+            憧れの職業へ
+          </span>
         </h1>
       </div>
     </div>
