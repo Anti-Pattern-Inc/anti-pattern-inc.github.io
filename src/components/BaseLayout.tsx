2e80ea5c10f46554051b8dc4b4f3f6ffc9ddb310
ryudai <bigdragon.06102001@gmail.com>
2023-01-29 09:18:38 +0900

    newsページにBaseLayoutの導入

--- src/components/BaseLayout.tsx
+++ src/components/BaseLayout.tsx
@@ -4,15 +4,18 @@ import { ReactNode } from 'react'
 
 type Props = {
   children: ReactNode
+  headerBgColor?: string
 }
 
-const BaseLayout = ({ children }: Props) => {
+const BaseLayout = ({ children, headerBgColor = 'white' }: Props) => {
   return (
-    <>
-      <Header />
+    <div className="min-h-screen flex flex-col justify-between">
+      <div className={`bg-${headerBgColor}`}>
+        <Header />
+      </div>
       <main>{children}</main>
       <Footer />
-    </>
+    </div>
   )
 }
 
