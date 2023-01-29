bd73601980eca8711c15ba30d6e2c39aaa69627e
ryudai <bigdragon.06102001@gmail.com>
2023-01-29 09:30:28 +0900

    プライバシーポリシーページにBaseLayoutの導入

--- pages/legal/index.tsx
+++ pages/legal/index.tsx
@@ -1,6 +1,5 @@
 import { SliceZone } from '@prismicio/react'
-import { Footer } from 'components/Footer'
-import { Header } from 'components/Header'
+import { BaseLayout } from 'components/BaseLayout'
 import { GetStaticPropsContext, InferGetStaticPropsType } from 'next'
 import Head from 'next/head'
 
@@ -21,27 +20,27 @@ export default function Legal({ legal_policy }: PageProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <main className="bg-slate-50">
+      <BaseLayout headerBgColor="stone-100">
         <div className="bg-stone-100 pb-10 md:pb-20">
-          <Header />
-          <h1 className="text-4xl font-bold sm:text-5xl lg:text-6xl text-center mt-8 md:mt-24">
+          <h1 className="text-4xl font-bold sm:text-5xl lg:text-6xl text-center pt-8 md:pt-24">
             LEGAL POLICY
           </h1>
           <p className="font-bold text-center mt-6 md:mt-10">
             個人情報の取り扱い等
           </p>
         </div>
-        <article className="mx-6 px-6 mt-10 pt-6 mb-24 pb-24 max-w-6xl xl:mx-auto lg:px-32 md:mt-16 md:pt-16 md:mb-36 md:pb-36 bg-white">
-          <p className="text-ap-green text-sm"></p>
-          <section className="pt-6 md:pt-12">
-            <SliceZone
-              slices={legal_policy.data.slices}
-              components={components}
-            />
-          </section>
-        </article>
-        <Footer />
-      </main>
+        <div className="bg-slate-50 pt-10 pb-24 md:pt-16 md:pb-36">
+          <article className="mx-6 px-6 pt-6 pb-24 max-w-6xl xl:mx-auto lg:px-32 md:pt-16 md:pb-36 bg-white">
+            <p className="text-ap-green text-sm"></p>
+            <section className="pt-6 md:pt-12">
+              <SliceZone
+                slices={legal_policy.data.slices}
+                components={components}
+              />
+            </section>
+          </article>
+        </div>
+      </BaseLayout>
     </>
   )
 }
