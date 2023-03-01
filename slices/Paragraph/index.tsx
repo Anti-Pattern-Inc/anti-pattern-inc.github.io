59e864128a2dd50bab8739fecd80b3d412b70c8e
ryudai <bigdragon.06102001@gmail.com>
2023-03-01 13:40:39 +0900

    条件分岐をswitch文に置き換え

--- slices/Paragraph/index.tsx
+++ slices/Paragraph/index.tsx
@@ -5,41 +5,45 @@ import type { SliceComponentProps } from '@prismicio/react'
 
 export type ParagraphProps = SliceComponentProps<Content.ParagraphSlice>
 
-const Paragraph = ({ slice }: ParagraphProps) => (
-  <>
-    {(slice.variation === 'default' && (
-      <section className="paragraph">
-        <div>
-          {slice.items.map((item, key) => {
-            return (
-              <div
-                key={key}
-                className={
-                  key > 0 ? 'border-t-4 border-stone-100 mt-16 pt-12' : ''
-                }
-              >
-                <PrismicRichText field={item.content}></PrismicRichText>
-              </div>
-            )
-          })}
-        </div>
-      </section>
-    )) ||
-      (slice.variation === 'interview' && (
-        <section className="interview">
+const Paragraph = ({ slice }: ParagraphProps) => {
+  switch (slice.variation) {
+    case 'default':
+      return (
+        <section className="paragraph">
           <div>
             {slice.items.map((item, key) => {
               return (
-                <div key={key}>
-                  <h3>{item.title}</h3>
+                <div
+                  key={key}
+                  className={
+                    key > 0 ? 'border-t-4 border-stone-100 mt-16 pt-12' : ''
+                  }
+                >
                   <PrismicRichText field={item.content}></PrismicRichText>
                 </div>
               )
             })}
           </div>
         </section>
-      ))}
-  </>
-)
+      )
+    case 'interview':
+      return (
+        slice.variation === 'interview' && (
+          <section className="interview">
+            <div>
+              {slice.items.map((item, key) => {
+                return (
+                  <div key={key}>
+                    <h3>{item.title}</h3>
+                    <PrismicRichText field={item.content}></PrismicRichText>
+                  </div>
+                )
+              })}
+            </div>
+          </section>
+        )
+      )
+  }
+}
 
 export default Paragraph
