1161bb56ac49ef39224e7ca3528025b26552205c
Atsuhiro Nakai <106391644+atsuh1r0@users.noreply.github.com>
2023-06-09 18:45:25 +0900

    add: top-sitemapにページを追加

--- src/pages/api/sitemap-top.ts
+++ src/pages/api/sitemap-top.ts
@@ -2,13 +2,23 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { SitemapStream, streamToPromise } from 'sitemap'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const links = [
+    { url: '/' },
+    { url: '/services' },
+    { url: '/services/saasus-consulting' },
+    { url: '/company' },
+    { url: '/legal' },
+  ]
+
   const stream = new SitemapStream({
     hostname:
       process.env.NEXT_PUBLIC_ANTIPATTERN_CORPORATE_SITE_URL ||
       'https://anti-pattern.co.jp',
   })
 
-  stream.write({ url: '/' })
+  links.forEach((link) => {
+    stream.write(link)
+  })
 
   stream.end()
 
