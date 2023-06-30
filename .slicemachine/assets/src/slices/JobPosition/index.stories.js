f2f4d79a25f0886e6644f48fac23b1d23ab7a2bc
Atsuhiro Nakai <106391644+atsuh1r0@users.noreply.github.com>
2023-06-30 11:55:31 +0900

    add: JObPosition スライス、カスタムタイプ追加

---
+++ .slicemachine/assets/src/slices/JobPosition/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../../src/slices/JobPosition';
+
+export default {
+  title: 'src/slices/JobPosition'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"positionname":[{"type":"heading1","text":"Article","spans":[]}],"description":[{"type":"paragraph","text":"Est do incididunt sunt nulla officia. Minim ad quis exercitation aliquip commodo enim enim laborum irure. Minim fugiat sint ut tempor excepteur ut dolore veniam ipsum Lorem anim officia aliquip.","spans":[]}],"image":{"dimensions":{"width":4380,"height":2895},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1537498425277-c283d32ef9db"}},"id":"_Default","slice_type":"job_position"}} />
+_Default.storyName = ''
