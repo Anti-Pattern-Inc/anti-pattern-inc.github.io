059edbedb9968549b5345d160a88b26a5331f645
Atsuhiro Nakai <106391644+atsuh1r0@users.noreply.github.com>
2023-06-30 16:43:23 +0900

    fix: hero レスポンシブ対応

--- src/components/career/Hero.tsx
+++ src/components/career/Hero.tsx
@@ -3,13 +3,13 @@ import Image from 'next/image'
 
 const Hero = () => {
   return (
-    <div className="relative h-[520px] w-full overflow-x-clip">
+    <div className="relative h-[400px] sm:h-[520px] w-full overflow-x-clip">
       <div className="absolute inset-0">
         <Image src={HeroImg} alt="hero_img" className="h-full object-cover" />
       </div>
-      <div className="relative mx-auto max-w-7xl px-6 lg:px-8 pt-52">
+      <div className="absolute flex items-center h-full mx-auto max-w-7xl px-6 lg:px-8">
         <h2>
-          <span className="text-white block text-xl sm:text-4xl font-bold">
+          <span className="text-white block text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold">
             「ソフトウェアエンジニアリングを通じて
             <br />
             人や企業の成長を促進し、日本の発展を加速させる」
