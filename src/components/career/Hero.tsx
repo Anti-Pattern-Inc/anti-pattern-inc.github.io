f14bef9d04d3d3a1cfcca8df56954747bda1cae0
Atsuhiro Nakai <106391644+atsuh1r0@users.noreply.github.com>
2023-06-30 17:01:02 +0900

    add: html構成を変更

--- src/components/career/Hero.tsx
+++ src/components/career/Hero.tsx
@@ -8,17 +8,16 @@ const Hero = () => {
         <Image src={HeroImg} alt="hero_img" className="h-full object-cover" />
       </div>
       <div className="absolute flex items-center h-full mx-auto max-w-7xl px-6 lg:px-8">
-        <h2>
-          <span className="text-white block text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold">
+        <div>
+          <p className="text-white block text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold">
             「ソフトウェアエンジニアリングを通じて
             <br />
             人や企業の成長を促進し、日本の発展を加速させる」
-            <br />
-            <span className="block mt-10">
-              アンチパターンでは一緒に働く仲間を募集しています。
-            </span>
-          </span>
-        </h2>
+          </p>
+          <h1 className="text-white block text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold mt-10">
+            アンチパターンでは一緒に働く仲間を募集しています。
+          </h1>
+        </div>
       </div>
     </div>
   )
