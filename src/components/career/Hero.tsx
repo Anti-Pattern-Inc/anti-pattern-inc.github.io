6ac2dfee6a9cbf4c519a212258cf2897e7da4159
Atsuhiro Nakai <106391644+atsuh1r0@users.noreply.github.com>
2023-07-06 20:10:01 +0900

    change: ヒーローの文字を右にする

--- src/components/career/Hero.tsx
+++ src/components/career/Hero.tsx
@@ -8,8 +8,8 @@ const Hero = () => {
         <Image src={HeroImg} alt="hero_img" className="h-full object-cover" />
       </div>
       <div className="absolute inset-0 bg-black bg-opacity-30"></div>
-      <div className="absolute flex items-center h-full mx-auto max-w-7xl px-6 lg:px-8">
-        <div>
+      <div className="absolute flex items-center h-full w-full mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto">
           <p className="text-white block text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold">
             「ソフトウェアエンジニアリングを通じて
             <br />
