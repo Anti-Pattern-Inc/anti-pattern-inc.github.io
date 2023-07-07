e28ebf7fb89febf69006ff81459d8999485aba21
Atsuhiro Nakai <106391644+atsuh1r0@users.noreply.github.com>
2023-07-07 14:30:22 +0900

    change: 採用フロー修正

--- src/components/career/RecruitingFlow.tsx
+++ src/components/career/RecruitingFlow.tsx
@@ -4,24 +4,24 @@ const RecruitingFlow = () => {
       <h2 className="mb-4 md:mb-8 font-bold text-ap-green text-2xl text-center">
         採用フロー
       </h2>
-      <div className="flex justify-center items-center flex-col md:flex-row gap-2 w-4/5 md:w-full m-auto px-4">
-        <div className="flex items-center justify-center h-40 text-2xl font-bold bg-ap-green bg-opacity-40 w-full md:w-56 text-center rounded-2xl">
+      <div className="flex justify-center items-center flex-col md:flex-row gap-4 w-4/5 md:w-full m-auto px-4">
+        <div className="flex items-center justify-center h-40 text-2xl font-bold bg-ap-green bg-opacity-10 w-full md:w-56 text-center rounded-2xl px-1">
           <p>カジュアル面談</p>
         </div>
-        <span className="h-6 border-l-[48px] border-t-[24px] border-b-[24px] box-border border-y-transparent border-l-ap-green transform rotate-90 md:rotate-0"></span>
-        <div className="flex items-center justify-center h-40 text-2xl font-bold bg-ap-green bg-opacity-40 w-full md:w-56 text-center rounded-2xl">
+        <span className="h-6 border-l-[28px] border-t-[14px] border-b-[14px] box-border border-y-transparent border-l-ap-green transform rotate-90 md:rotate-0"></span>
+        <div className="flex items-center justify-center h-40 text-2xl font-bold bg-ap-green bg-opacity-10 w-full md:w-56 text-center rounded-2xl px-1">
           <p>応募意思確認</p>
         </div>
-        <span className="h-6 border-l-[48px] border-t-[24px] border-b-[24px] box-border border-y-transparent border-l-ap-green transform rotate-90 md:rotate-0"></span>
-        <div className="flex items-center justify-center h-40 text-2xl font-bold bg-ap-green bg-opacity-40 w-full md:w-56 text-center rounded-2xl">
+        <span className="h-6 border-l-[28px] border-t-[14px] border-b-[14px] box-border border-y-transparent border-l-ap-green transform rotate-90 md:rotate-0"></span>
+        <div className="flex items-center justify-center h-40 text-2xl font-bold bg-ap-green bg-opacity-10 w-full md:w-56 text-center rounded-2xl px-1">
           <p>
             面接
             <br />
             （2〜4回）
           </p>
         </div>
-        <span className="h-6 border-l-[48px] border-t-[24px] border-b-[24px] box-border border-y-transparent border-l-ap-green transform rotate-90 md:rotate-0"></span>
-        <div className="flex items-center justify-center h-40 text-2xl font-bold bg-ap-green bg-opacity-40 w-full md:w-56 text-center rounded-2xl">
+        <span className="h-6 border-l-[28px] border-t-[14px] border-b-[14px] box-border border-y-transparent border-l-ap-green transform rotate-90 md:rotate-0"></span>
+        <div className="flex items-center justify-center h-40 text-2xl font-bold bg-ap-green bg-opacity-10 w-full md:w-56 text-center rounded-2xl px-1">
           <p>内定</p>
         </div>
       </div>
