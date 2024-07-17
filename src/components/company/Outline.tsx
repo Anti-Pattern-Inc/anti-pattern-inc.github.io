e08c13047b51a188fb0445191934b7ce45a83413
Sakinom <sakinom328@gmail.com>
2024-07-17 22:07:21 +0900

    style: :fire: 不要なreturnを削除

--- src/components/company/Outline.tsx
+++ src/components/company/Outline.tsx
@@ -12,20 +12,18 @@ type Props = {
 const OutlineFrag = ({ outline }: Props) => {
   return (
     <div>
-      {outline.map((item, key) => {
-        return (
-          <div key={key} className="border-b-2 border-stone-200">
-            <div className="h-28 max-w-sm lg:max-w-none mx-auto grid grid-cols-5 items-center px-6">
-              <p className="col-span-2 lg:col-span-1 text-sm font-bold pl-3">
-                {item.title}
-              </p>
-              <p className="col-span-3 lg:col-span-4 text-sm font-bold pr-3">
-                {item.content}
-              </p>
-            </div>
+      {outline.map((item, key) => (
+        <div key={key} className="border-b-2 border-stone-200">
+          <div className="h-28 max-w-sm lg:max-w-none mx-auto grid grid-cols-5 items-center px-6">
+            <p className="col-span-2 lg:col-span-1 text-sm font-bold pl-3">
+              {item.title}
+            </p>
+            <p className="col-span-3 lg:col-span-4 text-sm font-bold pr-3">
+              {item.content}
+            </p>
           </div>
-        )
-      })}
+        </div>
+      ))}
     </div>
   )
 }
