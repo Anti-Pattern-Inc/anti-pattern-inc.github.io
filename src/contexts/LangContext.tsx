ac6569166364e4619675c771ad08d225fd319159
Sakinom <sakinom328@gmail.com>
2024-07-17 13:01:36 +0900

    feat: :sparkles: LanguageContextProviderに値を渡す

--- src/contexts/LangContext.tsx
+++ src/contexts/LangContext.tsx
@@ -1,11 +1,25 @@
 import { LANGUAGE_TYPE } from 'const/language-type'
-import { createContext, useContext } from 'react'
+import { useRouter } from 'next/router'
+import { createContext, ReactNode, useEffect, useState } from 'react'
 
 export const LangContext = createContext<{
   lang: LANGUAGE_TYPE
 }>({ lang: LANGUAGE_TYPE.JAPANESE })
 
-export const useLanguage = () => {
-  const { lang } = useContext(LangContext)
-  return lang
+type LangContextProviderProps = {
+  children?: ReactNode
+}
+
+export const LangContextProvider = ({ children }: LangContextProviderProps) => {
+  const router = useRouter()
+  const locale = router.locale
+  const [lang, setLang] = useState<LANGUAGE_TYPE>(LANGUAGE_TYPE.JAPANESE)
+
+  useEffect(() => {
+    setLang(locale === 'ja' ? LANGUAGE_TYPE.JAPANESE : LANGUAGE_TYPE.ENGLISH)
+  }, [locale])
+
+  return (
+    <LangContext.Provider value={{ lang }}>{children}</LangContext.Provider>
+  )
 }
