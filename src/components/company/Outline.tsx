d257029aa756a27f50f40085d5b4e3269a9a75cd
Sakinom <sakinom328@gmail.com>
2024-07-18 00:02:09 +0900

    style: :art: 言語が日本語の時と英語の時で幅を変更することで文字の重なりを解消

--- src/components/company/Outline.tsx
+++ src/components/company/Outline.tsx
@@ -1,4 +1,5 @@
 import { useLocale } from 'hooks/useLocale'
+import { useRouter } from 'next/router'
 
 type OutlineContent = {
   title: string
@@ -10,15 +11,26 @@ type Props = {
 }
 
 const OutlineFrag = ({ outline }: Props) => {
+  const router = useRouter()
+  const locale = router.locale
+
   return (
     <div>
       {outline.map((item, key) => (
         <div key={key} className="border-b-2 border-stone-200">
-          <div className="h-28 max-w-sm lg:max-w-none mx-auto grid grid-cols-5 items-center px-6">
-            <p className="col-span-2 lg:col-span-1 text-sm font-bold pl-3">
+          <div className="h-28 max-w-sm lg:max-w-none mx-auto grid grid-cols-6 items-center px-6">
+            <p
+              className={`col-span-3 ${
+                locale === 'ja' ? 'lg:col-span-2' : 'lg:col-span-3'
+              } text-sm font-bold pl-3`}
+            >
               {item.title}
             </p>
-            <p className="col-span-3 lg:col-span-4 text-sm font-bold pr-3">
+            <p
+              className={`col-span-3 ${
+                locale === 'ja' ? 'lg:col-span-4' : 'lg:col-span-3'
+              } text-sm font-bold pr-3`}
+            >
               {item.content}
             </p>
           </div>
