472653191844e35e8eb2c6218e68c7e0b15ba306
Sakinom <sakinom328@gmail.com>
2024-07-18 15:51:39 +0900

    feat: :art: サービスページの日本語のサブタイトルを日本語ページでのみ表示

--- src/pages/services/index.tsx
+++ src/pages/services/index.tsx
@@ -2,9 +2,12 @@ import { CustomHead } from 'components/common/CustomHead'
 import { BusinessConcept } from 'components/features/BusinessConcept'
 import { ServiceList } from 'components/features/ServiceList'
 import { BaseLayout } from 'components/layouts/BaseLayout'
+import { LANGUAGE_TYPE } from 'const/language-type'
+import { useLanguage } from 'contexts/LangContext'
 import { useLocale } from 'hooks/useLocale'
 
 const Services = () => {
+  const lang = useLanguage()
   const t = useLocale()
 
   return (
@@ -18,7 +21,9 @@ const Services = () => {
           <h1 className="text-4xl font-bold sm:text-5xl lg:text-6xl text-center pt-8 md:pt-24">
             SERVICES
           </h1>
-          <p className="font-bold text-center mt-6 md:mt-10">サービス</p>
+          {lang === LANGUAGE_TYPE.JAPANESE && (
+            <p className="font-bold text-center mt-6 md:mt-10">サービス</p>
+          )}
         </div>
         <BusinessConcept />
         <ServiceList />
