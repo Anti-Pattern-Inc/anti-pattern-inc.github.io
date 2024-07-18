b8eff2862fef55bb061a76e311998d72e6b417a5
Sakinom <sakinom328@gmail.com>
2024-07-18 15:47:08 +0900

    feat: :sparkles: services.jsonファイルがuseLocale()で使えるようにオブジェクトに追加

--- src/hooks/useLocale.tsx
+++ src/hooks/useLocale.tsx
@@ -2,14 +2,18 @@ import { LANGUAGE_TYPE } from 'const/language-type'
 import { useLanguage } from 'contexts/LangContext'
 
 import * as companyEn from '../../public/locales/en/company.json'
+import * as servicesEn from '../../public/locales/en/services.json'
 import * as company from '../../public/locales/ja/company.json'
+import * as services from '../../public/locales/ja/services.json'
 
 const ja = {
   company: company,
+  services: services,
 }
 
 const en = {
   company: companyEn,
+  services: servicesEn,
 }
 
 export const useLocale = () => {
