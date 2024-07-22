75ec534f9b49a30114d1c91bdea573bc3a47ff4c
Sakinom <sakinom328@gmail.com>
2024-07-23 08:53:26 +0900

    refactor: :art: 日本語版翻訳jsonファイルを明示的に日本語だとわかるようにフックでの命名を変更

--- src/hooks/useLocale.tsx
+++ src/hooks/useLocale.tsx
@@ -2,10 +2,10 @@ import { LANGUAGE_TYPE } from 'const/language-type'
 import { useLanguage } from 'contexts/LangContext'
 
 import * as companyEn from '../../public/locales/en/company.json'
-import * as company from '../../public/locales/ja/company.json'
+import * as companyJa from '../../public/locales/ja/company.json'
 
 const ja = {
-  company: company,
+  company: companyJa,
 }
 
 const en = {
