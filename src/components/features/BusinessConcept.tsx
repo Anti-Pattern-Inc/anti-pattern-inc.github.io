40de546d1fc669bb78aaeddc6be6c85d401c30b8
Sakinom <sakinom328@gmail.com>
2024-10-01 15:01:59 +0900

    feat: :sparkles: サービスページの画像を英語版と日本語版とで切り替えるように変更

--- src/components/features/BusinessConcept.tsx
+++ src/components/features/BusinessConcept.tsx
@@ -1,8 +1,13 @@
+import { useLocale } from 'hooks/useLocale'
 import ServiceImg from 'images/anti-pattern_service_2022.png'
+import ServiceImgEn from 'images/anti-pattern_service_2022-en.png'
 import ServiceImgSp from 'images/anti-pattern_service_2022-sp.png'
+import ServiceImgSpEn from 'images/anti-pattern_service_2022-sp-en.png'
 import Image from 'next/image'
 
 const BusinessConcept = () => {
+  const { lang } = useLocale()
+
   return (
     <section className="mt-16 md:mt-32">
       <h2 className="mb-4 md:mb-8 font-bold text-ap-green text-2xl text-center">
@@ -10,12 +15,12 @@ const BusinessConcept = () => {
       </h2>
       <div className="max-w-md lg:max-w-6xl mx-auto px-10">
         <Image
-          src={ServiceImg}
+          src={lang === 'ja' ? ServiceImg : ServiceImgEn}
           alt="ビジネスコンセプト"
           className="hidden lg:block"
         />
         <Image
-          src={ServiceImgSp}
+          src={lang === 'ja' ? ServiceImgSp : ServiceImgSpEn}
           alt="ビジネスコンセプト"
           className="lg:hidden"
         />
