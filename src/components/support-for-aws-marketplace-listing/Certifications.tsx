6cc448df5cb0b941765c579119f5e50917fcda00
Atsuhiro Nakai <106391644+atsuh1r0@users.noreply.github.com>
2025-01-25 15:28:26 +0900

    feat: :art: レスポンシブ対応

--- src/components/support-for-aws-marketplace-listing/Certifications.tsx
+++ src/components/support-for-aws-marketplace-listing/Certifications.tsx
@@ -4,18 +4,40 @@ import Image from 'next/image'
 
 const Certifications = () => {
   return (
-    <div className="flex flex-col items-center justify-center gap-5 py-20">
-      <h2 className="text-[34px] text-[#0277BD] font-bold">Certifications</h2>
-      <div className="flex gap-20">
+    <div className="flex flex-col items-center justify-center gap-6 md:gap-10 py-10 md:py-20">
+      <h2 className="text-2xl md:text-[34px] md:leading-tight text-[#0277BD] font-bold">
+        Certifications
+      </h2>
+      <div className="flex gap-6 md:gap-20 flex-col md:flex-row">
         <Image
           src={SaaSCompetency}
           alt="saas-competency"
           width={385}
           height={170}
-          className="h-full"
+          className="h-full hidden lg:block"
         />
-        <div className="flex flex-col items-center gap-3">
-          <Image src={ISMS} alt="isms" width={335} height={170} className="" />
+        <Image
+          src={SaaSCompetency}
+          alt="saas-competency"
+          width={280}
+          height={124}
+          className="h-full lg:hidden"
+        />
+        <div className="flex flex-col items-center gap-2 md:gap-3">
+          <Image
+            src={ISMS}
+            alt="isms"
+            width={335}
+            height={170}
+            className="hidden lg:block"
+          />
+          <Image
+            src={ISMS}
+            alt="isms"
+            width={280}
+            height={142}
+            className="lg:hidden"
+          />
           <div className="text-xl leading-8">IS 794005</div>
         </div>
       </div>
