aa4e7d63ffa141bd0bf1b66a6edb4deb48796cc3
Atsuhiro Nakai <106391644+atsuh1r0@users.noreply.github.com>
2025-02-07 15:28:54 +0900

    feat: :art: サービスサイト英語版画像差し替え対応

--- src/components/features/ServiceList.tsx
+++ src/components/features/ServiceList.tsx
@@ -8,19 +8,9 @@ import Posse from 'images/posse.svg'
 import SaasusConsulting from 'images/saasus_consulting_logo_420.svg'
 import SaasusPlatform from 'images/saasus_platform_logo_420.svg'
 import ServiceLinkIcon from 'images/service-link-icon.png'
-import SupportForAwsMarketplaceListing from 'images/support_for_aws_marketplace_listing.svg'
+import SupportForAwsMarketplaceListing from 'images/support_for_aws_marketplace_listing.png'
+import SupportForAwsMarketplaceListingEn from 'images/support_for_aws_marketplace_listing_en.png'
 import Image, { StaticImageData } from 'next/image'
-
-const images: { [key: string]: StaticImageData } = {
-  Engineed: Engineed,
-  SaasusPlatform: SaasusPlatform,
-  SaasusConsulting: SaasusConsulting,
-  SupportForAwsMarketplaceListing: SupportForAwsMarketplaceListing,
-  Gateway: Gateway,
-  HarborS: HarborS,
-  Posse: Posse,
-  CloudDriver: CloudDriver,
-}
 
 type Props = {
   src: StaticImageData
@@ -72,7 +62,21 @@ const Service = ({ src, name, ja_name, description, url, isBlank }: Props) => {
 }
 
 const ServiceList = () => {
-  const { t } = useLocale()
+  const { t, lang } = useLocale()
+
+  const images: { [key: string]: StaticImageData } = {
+    Engineed: Engineed,
+    SaasusPlatform: SaasusPlatform,
+    SaasusConsulting: SaasusConsulting,
+    SupportForAwsMarketplaceListing:
+      lang === LANGUAGE_TYPE.JAPANESE
+        ? SupportForAwsMarketplaceListing
+        : SupportForAwsMarketplaceListingEn,
+    Gateway: Gateway,
+    HarborS: HarborS,
+    Posse: Posse,
+    CloudDriver: CloudDriver,
+  }
 
   return (
     <section className="mt-32 mb-32">
