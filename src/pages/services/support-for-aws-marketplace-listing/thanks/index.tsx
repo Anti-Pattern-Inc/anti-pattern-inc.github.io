bd3afb39ea3eaca35d88090324490ea0c70e4563
Atsuhiro Nakai <106391644+atsuh1r0@users.noreply.github.com>
2025-02-14 07:53:43 +0900

    refactor: :recycle: titleタグの文言変更

--- src/pages/services/support-for-aws-marketplace-listing/thanks/index.tsx
+++ src/pages/services/support-for-aws-marketplace-listing/thanks/index.tsx
@@ -6,7 +6,7 @@ const MarketplaceSupport: React.FC = () => {
   return (
     <>
       <CustomHead
-        title="送信完了 - AWS Marketplace 掲載支援サービス | Anti-Pattern Inc."
+        title="送信が完了しました - AWS Marketplace 掲載支援サービス | Anti-Pattern Inc."
         description="AWSアドバンストティアパートナー及び、SaaSコンピテンシーパートナーとして、お客様のSaaS製品をAWS Marketplaceにスムーズに掲載するためのサービスを提供しております。SaaS提供事業者として、自社SaaS製品を掲載した経験と実績に基づいたノウハウで、AWS Marketplaceへ掲載できる状態のプロダクト開発を支援し、販路拡大に寄与いたします。"
       />
       <BaseLayout headerBgColor="stone-100">
