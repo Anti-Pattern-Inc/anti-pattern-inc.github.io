a0d04ffc96929c6e4fc09d4552513bce1dcf0caa
Atsuhiro Nakai <106391644+atsuh1r0@users.noreply.github.com>
2025-02-14 07:47:00 +0900

    feat: :art: thanksページのレスポンシブ対応

--- src/components/support-for-aws-marketplace-listing/Thanks.tsx
+++ src/components/support-for-aws-marketplace-listing/Thanks.tsx
@@ -6,11 +6,11 @@ import Link from 'next/link'
 
 const Thanks = () => {
   return (
-    <section className="flex flex-col items-center gap-10 py-20">
+    <section className="flex flex-col items-center gap-6 md:gap-10 py-10 md:py-20 mx-10">
       <Image
         src={ThanksHero}
         alt="Thank you for your inquiry"
-        className="w-[500px] lg:h-[244px]"
+        className="max-w-[500px] w-full"
       />
       <h1 className="text-[#01579B] text-2xl md:text-[34px] md:leading-tight font-bold">
         送信が完了しました
@@ -24,7 +24,7 @@ const Thanks = () => {
       </p>
       <Link
         href="/services/support-for-aws-marketplace-listing"
-        className="flex justify-center items-center gap-2 bg-[#EF6C00] text-white text-bold text-xl leading-8 py-4 px-10 rounded-full hover:bg-[#E65100]"
+        className="flex justify-center items-center gap-2 bg-[#EF6C00] text-white text-bold text-base md:text-xl md:leading-8 py-4 px-10 rounded-full hover:bg-[#E65100]"
       >
         <ArrowUturnLeftIcon className="h-6 w-6 inline" />
         サービスページに戻る
