c05c983ecf5b597c2fdbfd82da79db0216f04c98
kazuki1023 <kazuki.iwagi.002@layerx.co.jp>
2025-02-20 19:23:34 +0900

    ファイル場所を移動させた

--- src/pages/services/support-for-aws-marketplace-listing/index.tsx
+++ src/pages/services/support-for-aws-marketplace-listing/index.tsx
@@ -6,6 +6,7 @@ import { Certifications } from 'components/support-for-aws-marketplace-listing/C
 import { FAQ } from 'components/support-for-aws-marketplace-listing/FAQ'
 import { Hero } from 'components/support-for-aws-marketplace-listing/Hero'
 import { Strength } from 'components/support-for-aws-marketplace-listing/Strength'
+import { ListSupportingService } from 'components/support-for-aws-marketplace-listing/ListSupportingService'
 
 const MarketplaceSupport: React.FC = () => {
   return (
@@ -16,6 +17,7 @@ const MarketplaceSupport: React.FC = () => {
       />
       <BaseLayout headerBgColor="stone-100">
         <Hero />
+        <ListSupportingService />
         <AboutAWSMarketplace />
         <AboutSupportForAWSMarketplaceListing />
         <Strength />
