12e9d1aa31199cc6e65e0c1958fe44ff683caa24
kazuki1023 <kazuki.iwagi.002@layerx.co.jp>
2025-02-22 18:19:25 +0900

    sp対応した

--- src/components/support-for-aws-marketplace-listing/ContactDialog.tsx
+++ src/components/support-for-aws-marketplace-listing/ContactDialog.tsx
@@ -15,13 +15,14 @@ const ContactDialog = ({
       open={isOpen}
       // onCloseは必須Propなので記載。ただし閉じるボタンのみでDialogが閉じてほしいので、
       // ここでは何もしない
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
       onClose={() => {}}
       className="relative z-10"
     >
       <div className="fixed inset-0 bg-black/40" aria-hidden="true" />
       <div className="fixed inset-0 z-10 overflow-y-auto ">
         <div className="flex min-h-full p-4 items-center justify-center flex-grow-0 flex-shrink-0 w-full">
-          <Dialog.Panel className="w-[774px] bg-white p-6 shadow-lg flex flex-col items-center justify-center flex-grow-0 flex-shrink-0">
+          <Dialog.Panel className="w-[358px] md:w-[774px] bg-white p-6 shadow-lg flex flex-col items-center justify-center flex-grow-0 flex-shrink-0">
             <Dialog.Title className="text-4xl font-bold text-[#0277BD] mx-auto mb-10">
               お問い合わせ
             </Dialog.Title>
