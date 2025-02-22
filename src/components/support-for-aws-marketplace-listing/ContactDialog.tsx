fe0aa4bd35ac52e10cbc76ade3ed8b7dd958123a
kazuki1023 <kazuki.iwagi.002@layerx.co.jp>
2025-02-22 18:13:30 +0900

    Dialogの見た目完成

--- src/components/support-for-aws-marketplace-listing/ContactDialog.tsx
+++ src/components/support-for-aws-marketplace-listing/ContactDialog.tsx
@@ -1,6 +1,7 @@
 import { Dialog } from '@headlessui/react'
-import Image from 'next/image'
 import { Dispatch, SetStateAction } from 'react'
+
+import { ContactForm } from './Contact/Form'
 
 const ContactDialog = ({
   isOpen,
@@ -17,103 +18,14 @@ const ContactDialog = ({
       onClose={() => {}}
       className="relative z-10"
     >
-      <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
-      <div className="fixed inset-0 z-10 overflow-y-auto">
-        <div className="flex min-h-full items-center justify-center p-4">
-          <Dialog.Panel className="w-full max-w-md rounded-xl bg-white p-6 shadow-lg">
-            <Dialog.Title className="text-lg font-medium text-[#0277BD]">
+      <div className="fixed inset-0 bg-black/40" aria-hidden="true" />
+      <div className="fixed inset-0 z-10 overflow-y-auto ">
+        <div className="flex min-h-full p-4 items-center justify-center flex-grow-0 flex-shrink-0 w-full">
+          <Dialog.Panel className="w-[774px] bg-white p-6 shadow-lg flex flex-col items-center justify-center flex-grow-0 flex-shrink-0">
+            <Dialog.Title className="text-4xl font-bold text-[#0277BD] mx-auto mb-10">
               お問い合わせ
             </Dialog.Title>
-            <form className="mt-4 space-y-4">
-              <div>
-                <label className="block text-sm font-medium text-gray-700">
-                  メールアドレス<span className="text-red-500">*</span>
-                </label>
-                <input
-                  type="email"
-                  className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
-                  placeholder="info@anti-pattern.co.jp"
-                  required
-                />
-              </div>
-              <div>
-                <label className="block text-sm font-medium text-gray-700">
-                  会社名<span className="text-red-500">*</span>
-                </label>
-                <input
-                  type="text"
-                  className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
-                  placeholder="株式会社アンチパターン"
-                  required
-                />
-              </div>
-              <div>
-                <label className="block text-sm font-medium text-gray-700">
-                  電話番号<span className="text-red-500">*</span>
-                </label>
-                <input
-                  type="tel"
-                  className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
-                  placeholder="0368856136"
-                  required
-                />
-              </div>
-              <div className="flex space-x-2">
-                <div>
-                  <label className="block text-sm font-medium text-gray-700">
-                    姓<span className="text-red-500">*</span>
-                  </label>
-                  <input
-                    type="text"
-                    className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
-                    placeholder="小笹"
-                    required
-                  />
-                </div>
-                <div>
-                  <label className="block text-sm font-medium text-gray-700">
-                    名<span className="text-red-500">*</span>
-                  </label>
-                  <input
-                    type="text"
-                    className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
-                    placeholder="佑京"
-                    required
-                  />
-                </div>
-              </div>
-              <div>
-                <label className="block text-sm font-medium text-gray-700">
-                  お問い合わせ内容<span className="text-red-500">*</span>
-                </label>
-                <textarea
-                  className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
-                  placeholder="お問い合わせ内容を入力してください"
-                  required
-                ></textarea>
-              </div>
-              <div className="flex justify-center gap-4">
-                <button
-                  type="button"
-                  className="bg-gray-500 text-white py-2 px-4 rounded-full hover:bg-gray-600"
-                  onClick={() => setIsOpen(false)}
-                >
-                  キャンセル
-                </button>
-                <button
-                  type="submit"
-                  className="bg-orange-500 text-white py-2 px-4 rounded-full hover:bg-orange-600 flex items-center gap-2"
-                >
-                  <Image
-                    src="/send-icon.png"
-                    alt="送信"
-                    width={24}
-                    height={24}
-                  />
-                  送信
-                </button>
-              </div>
-            </form>
+            <ContactForm setIsOpen={setIsOpen} />
           </Dialog.Panel>
         </div>
       </div>
