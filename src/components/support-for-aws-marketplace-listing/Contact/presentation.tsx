3178183f260178f03513d1c96cd5efa75a773360
kazuki1023 <kazuki.iwagi.002@layerx.co.jp>
2025-02-22 20:45:57 +0900

    errorsFieldの表示のスタイル修正

--- src/components/support-for-aws-marketplace-listing/Contact/presentation.tsx
+++ src/components/support-for-aws-marketplace-listing/Contact/presentation.tsx
@@ -19,57 +19,63 @@ const FormPresentation: React.FC<FormProps> = ({
   setIsOpen,
 }) => {
   return (
-    <form className="mt-4 space-y-4 md:w-[648px] w-[310px]">
+    <form
+      className="mt-4 space-y-4 md:w-[648px] w-[310px]"
+      onSubmit={onSubmit}
+    >
       <div className="flex justify-between md:items-center gap-2 md:gap-4 flex-col md:flex-row">
         <label className="block text-sm md:text-base font-medium text-gray-700">
           メールアドレス<span className="text-red-500">*</span>
         </label>
-        <input
-          type="email"
-          className="block w-full border border-gray-300 rounded-md md:max-w-[500px] focus:outline-none h-14 placeholder:text-[black/60] placeholder:text-base placeholder:pl-2"
+        <div className="flex flex-col flex-1 md:max-w-[500px]">
+          <input
+            type="email"
+            className="block w-full border border-gray-300 rounded-md focus:outline-none h-14 placeholder:text-[black/60] placeholder:text-base placeholder:pl-2"
           placeholder="info@anti-pattern.co.jp"
-          required
           {...register('email')}
         />
         {errors?.email && (
-          <p className="text-[#D32F2F] text-xs">
+          <span className="text-[#D32F2F] text-xs">
             {errors.email.message?.toString()}
-          </p>
-        )}
+            </span>
+          )}
+        </div>
       </div>
       <div className="space-y-2 md:flex md:justify-between md:items-center md:gap-4">
         <label className="block text-sm md:text-base font-medium text-gray-700">
           会社名<span className="text-red-500">*</span>
         </label>
+        <div className="flex flex-col flex-1 md:max-w-[500px]">
         <input
           type="text"
-          className="block w-full border border-gray-300 rounded-md md:max-w-[500px] focus:outline-none h-14 placeholder:text-[black/60] placeholder:text-base placeholder:pl-2"
+          className="block w-full border border-gray-300 rounded-md focus:outline-none h-14 placeholder:text-[black/60] placeholder:text-base placeholder:pl-2"
           placeholder="株式会社アンチパターン"
-          required
-          {...register('companyName')}
-        />
-        {errors?.companyName && (
-          <p className="text-[#D32F2F] text-xs">
-            {errors.companyName.message?.toString()}
-          </p>
-        )}
+            {...register('companyName')}
+          />
+          {errors?.companyName && (
+            <p className="text-[#D32F2F] text-xs">
+              {errors.companyName.message?.toString()}
+            </p>
+          )}
+        </div>
       </div>
       <div className="space-y-2 md:flex md:justify-between md:items-center md:gap-4">
         <label className="block text-sm md:text-base font-medium text-gray-700">
           電話番号<span className="text-red-500">*</span>
         </label>
-        <input
-          type="tel"
-          className="block w-full border border-gray-300 rounded-md md:max-w-[500px] focus:outline-none h-14 placeholder:text-[black/60] placeholder:text-base placeholder:pl-2"
-          placeholder="0368856136"
-          required
-          {...register('phoneNumber')}
+        <div className="flex flex-col flex-1 md:max-w-[500px]">
+          <input
+            type="tel"
+            className="block w-full border border-gray-300 rounded-md focus:outline-none h-14 placeholder:text-[black/60] placeholder:text-base placeholder:pl-2"
+            placeholder="0368856136"
+            {...register('phoneNumber')}
         />
         {errors?.phoneNumber && (
           <p className="text-[#D32F2F] text-xs">
             {errors.phoneNumber.message?.toString()}
           </p>
         )}
+        </div>
       </div>
       <div className="space-y-2 md:flex md:justify-between md:items-center md:gap-4">
         <div className="flex flex-row gap-2">
@@ -82,73 +88,90 @@ const FormPresentation: React.FC<FormProps> = ({
           <label className="block text-sm md:text-base font-medium text-gray-700">
             性
           </label>
+          <div className="flex flex-col flex-1">
           <input
             type="text"
             className="block w-full border border-gray-300 rounded-md md:max-w-[500px] focus:outline-none h-14 placeholder:text-[black/60] placeholder:text-base placeholder:pl-2"
             placeholder="小笹"
-            required
             {...register('lastName')}
           />
           {errors?.lastName && (
             <p className="text-[#D32F2F] text-xs">
               {errors.lastName.message?.toString()}
             </p>
-          )}
+            )}
+          </div>
         </div>
         <div className="flex gap-4 items-center">
           <label className="block text-sm md:text-base font-medium text-gray-700">
             名
           </label>
+          <div className="flex flex-col flex-1">
           <input
             type="text"
             className="block w-full border border-gray-300 rounded-md md:max-w-[500px] focus:outline-none h-14 placeholder:text-[black/60] placeholder:text-base placeholder:pl-2"
             placeholder="佑京"
-            required
             {...register('firstName')}
           />
           {errors?.firstName && (
             <p className="text-[#D32F2F] text-xs">
               {errors.firstName.message?.toString()}
             </p>
-          )}
+            )}
+          </div>
         </div>
       </div>
       <div className="space-y-2 md:flex md:justify-between md:items-start md:gap-4">
         <label className="block text-sm md:text-base font-medium text-gray-700 md:pt-2">
           お問い合わせ内容<span className="text-red-500">*</span>
         </label>
+        <div className="flex flex-col flex-1">
         <textarea
           className="block w-full border border-gray-300 rounded-md md:max-w-[491px] focus:outline-none h-14 min-h-[160px]"
-          required
           {...register('inquiry')}
         ></textarea>
         {errors?.inquiry && (
           <p className="text-[#D32F2F] text-xs">
             {errors.inquiry.message?.toString()}
           </p>
-        )}
+          )}
+        </div>
       </div>
       <div
         className={`flex gap-4 items-start ${
           setIsOpen ? 'justify-between' : 'justify-center'
-        }`}
+        } `}
       >
         {setIsOpen && (
           <button
             type="button"
             className="py-2 px-4 text-[#2196F3] hover:bg-[#E3F2FD]"
             onClick={() => setIsOpen(false)}
+            disabled={isSubmitting}
           >
             キャンセル
           </button>
         )}
-        <button
-          type="submit"
-          className="bg-orange-500 text-white py-2 px-4 rounded-full hover:bg-orange-600 flex items-center gap-2"
-        >
-          <Image src="/send-icon.png" alt="送信" width={24} height={24} />
-          送信
-        </button>
+        {isSubmitting ? (
+          <button
+            type="submit"
+            className="bg-[#0000001F/12] text-white py-2 px-4 rounded-full hover:bg-orange-600 flex items-center gap-2"
+            disabled
+          >
+            <div className="inline-block w-6 h-6 animate-spin">
+              <Image src="/loading.svg" alt="送信" width={24} height={24} />
+            </div>
+            送信
+          </button>
+        ) : (
+          <button
+            type="submit"
+            className="bg-orange-500 text-white py-2 px-4 rounded-full hover:bg-orange-600 flex items-center gap-2"
+          >
+            <Image src="/mail-icon.png" alt="送信" width={24} height={24} />
+            送信
+          </button>
+        )}
       </div>
     </form>
   )
