6d5f433d7ad0a66eb85af5ecd14562a1ad88b6e5
kazuki1023 <kazuki.iwagi.002@layerx.co.jp>
2025-02-22 20:15:26 +0900

    責務を分離させた

---
+++ src/components/support-for-aws-marketplace-listing/Contact/container.tsx
@@ -0,0 +1,54 @@
+// ContainerComponent.tsx
+import { zodResolver } from '@hookform/resolvers/zod'
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+
+import { FormPresentation } from './presentation'
+
+const schema = z.object({
+  email: z
+    .string()
+    .email({ message: 'メールアドレスが不正です' })
+    .min(1, { message: '必須項目です' }),
+  companyName: z.string().min(1, { message: '必須項目です' }),
+  phoneNumber: z
+    .string()
+    .min(1, { message: '必須項目です' })
+    .regex(/^\d{10,11}$/, {
+      message:
+        '電話番号は、ハイフン（-）を入れずに10桁か11桁で記入してください',
+    }),
+  lastName: z.string().min(1, { message: '必須項目です' }),
+  firstName: z.string().min(1, { message: '必須項目です' }),
+  inquiry: z
+    .string()
+    .min(1, { message: '必須項目です' })
+    .max(32000, { message: '32000文字以内で入力してください' }),
+})
+
+const FormContainer = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    resolver: zodResolver(schema),
+  })
+
+  const onSubmit = (data: any) => {
+    // フォーム送信処理
+    console.log(data)
+  }
+
+  return (
+    <FormPresentation
+      onSubmit={handleSubmit(onSubmit)}
+      register={register}
+      errors={errors}
+      isSubmitting={isSubmitting}
+    />
+  )
+}
+
+export { FormContainer }
