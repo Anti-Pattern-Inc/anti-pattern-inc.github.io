3fc12230de5c07403f3687595e7ce643910357ac
Kazu <k.keio12569@gmail.com>
2025-02-24 19:38:11 +0900

    Update src/components/support-for-aws-marketplace-listing/Contact/Dialog.tsx
    Co-authored-by: ryudai <74942852+bigdragon0610@users.noreply.github.com>

--- src/components/support-for-aws-marketplace-listing/Contact/Dialog.tsx
+++ src/components/support-for-aws-marketplace-listing/Contact/Dialog.tsx
@@ -20,7 +20,7 @@ const _Dialog = ({
       className="relative z-50"
     >
       <div className="fixed inset-0 bg-black/40" aria-hidden="true" />
-      <div className="fixed inset-0 z-50 overflow-y-auto ">
+      <div className="fixed inset-0 z-50 overflow-y-auto">
         <div className="flex min-h-full p-4 items-center justify-center flex-grow-0 flex-shrink-0 w-full">
           <Dialog.Panel className="w-[358px] md:w-[774px] bg-white p-6 shadow-lg flex flex-col items-center justify-center flex-grow-0 flex-shrink-0">
             <Dialog.Title className="text-4xl font-bold text-[#0277BD] mx-auto mb-10">
